adc01d3498eea0522f7053a1a898fe6a4360d654
Josuah Aron <josuaharon@hotmail.com>
2014-12-17 11:00:38 +0100

    first commit

---
+++ src/fr/univpau/paupark/activity/PauParkActivity.java
@@ -0,0 +1,17 @@
+package fr.univpau.paupark.activity;
+
+import com.example.paupark.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PauParkActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pau_park);
+	}
+}
