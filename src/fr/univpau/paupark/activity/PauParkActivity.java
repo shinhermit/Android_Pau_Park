78b7f40ace1cd47f4324be35f57d507caae294fd
Josuah Aron <josuaharon@hotmail.com>
2014-12-17 12:23:01 +0100

    [Tabs] Adding tabs

--- src/fr/univpau/paupark/activity/PauParkActivity.java
+++ src/fr/univpau/paupark/activity/PauParkActivity.java
@@ -1,17 +1,29 @@
 package fr.univpau.paupark.activity;
 
-import com.example.paupark.R;
+import fr.univpau.paupark.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
 
-public class PauParkActivity extends Activity {
-
+public class PauParkActivity extends Activity
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pau_park);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater =
+				this.getMenuInflater();
+		
+		inflater.inflate(R.menu.settings_menu, menu);
+		
+		return super.onCreateOptionsMenu(menu);
 	}
 }
