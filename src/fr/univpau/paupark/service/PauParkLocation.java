b33987c6ccb10cf0b9a4e008eb8eff42cc63ab24
josuah <josuah.aron@gmail.com>
2015-01-17 16:11:20 +0100

    [Refining] Some few refinements.

--- src/fr/univpau/paupark/service/PauParkLocation.java
+++ src/fr/univpau/paupark/service/PauParkLocation.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import java.util.Observable;
 import java.util.Observer;
 
+import fr.univpau.paupark.R;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
@@ -13,6 +14,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 /**
  * Receives location updates.
  * Observable : observers notified of available location updates.
